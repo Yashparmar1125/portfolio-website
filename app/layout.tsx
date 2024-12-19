b8ab28a3239e6944b91913b9133bd1054678176b
Yash Parmar <yashparmar11y@gmail.com>
2024-12-19 23:01:00 +0530

    SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -16,8 +15,22 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Yash Parmar",
-  description: "Portfolio of Yash Parmar",
+  title: "Yash Parmar - Software Engineer | Portfolio & Projects",
+  description:
+    "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions. Discover projects, skills, and expertise.",
+  keywords: "Yash Parmar, software engineer, portfolio, web development, machine learning, tech solutions, projects, programming, software engineering, full-stack development",
+  author: "Yash Parmar",
+  openGraph: {
+    title: "Yash Parmar - Software Engineer | Portfolio & Projects",
+    description:
+      "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions. Discover projects, skills, and expertise.",
+    url: "https://yash-parmar.vercel.app/", // Replace with your actual URL
+    type: "website",
+     
+  },
+  icons: {
+    icon: "/favicon.ico", // Add your custom favicon here
+  },
 };
 
 export default function RootLayout({
@@ -27,10 +40,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-   
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" /> {/* Allows search engines to index the page */}
+        <meta name="theme-color" content="#000000" /> {/* Theme color for mobile browsers */}
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
         <Analytics />
       </body>
