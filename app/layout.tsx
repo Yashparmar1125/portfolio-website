f0ac43b582d9c8f7f45510a4d3ee5daf8aa8285d
Yash Parmar <yashparmar11y@gmail.com>
2024-12-19 23:23:58 +0530

    SEO optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,20 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Yash Parmar - Software Engineer | Portfolio & Projects",
   description:
-    "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions. Discover projects, skills, and expertise.",
-  keywords: "Yash Parmar, software engineer, portfolio, web development, machine learning, tech solutions, projects, programming, software engineering, full-stack development",
+    "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions.",
+  keywords: "Yash Parmar, software engineer, portfolio, web development, machine learning, tech solutions, Yash, AI, software engineering, full-stack development",
   
   openGraph: {
-    title: "Yash Parmar - Software Engineer | Portfolio & Projects",
+    title: "Yash Parmar - Software Engineer | Portfolio",
     description:
-      "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions. Discover projects, skills, and expertise.",
+      "Explore the portfolio of Yash Parmar, a passionate software engineer specializing in web development, machine learning, and innovative tech solutions. ",
     url: "https://yash-parmar.vercel.app/", // Replace with your actual URL
     type: "website",
      
   },
   icons: {
     icon: "/favicon.ico", // Add your custom favicon here
+    apple: "/apple-touch-icon.png",
   },
 };
 
@@ -42,7 +43,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="index, follow" /> {/* Allows search engines to index the page */}
         <meta name="theme-color" content="#000000" /> {/* Theme color for mobile browsers */}
       </head>
